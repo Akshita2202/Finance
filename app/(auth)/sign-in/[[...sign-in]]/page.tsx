46047e89b6ef416fe77b1509de4066c4c50da919
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2024-09-30 21:48:29 +0530

    clerk changes

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,18 @@
-import { SignIn } from '@clerk/nextjs'
+import { ClerkLoaded, ClerkLoading, SignIn } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
 
 export default function Page() {
-  return <SignIn path='sign-in' />
+  return (
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      
+  );
 }
