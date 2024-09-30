035a10a7adae46134b019d7e1275a758e313709f
Akshita2202 <singhakshita2012@gmail.com>
2024-09-30 21:34:22 +0530

    first commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+
+
+
+export default function Home() {
+  return (
+    
+    <Button>
+
+      <p>
+      This is an authenticated route
+      </p>
+      
+
+    </Button>
+  );
+}
